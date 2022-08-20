e2fbc2e58318154f189f48347c01e781d8a166c1
Diogo Fernandes <diogofernandesop@gmail.com>
2022-08-20 16:22:25 -0300

    Huge Updates

---
+++ README.pt-BR.md
@@ -0,0 +1,27 @@
+# Manga Reader
+Um simples e eficiente leitor de manga feito em JS
+
+[English](https://github.com/dfop02/manga-reader/README.md) | [Português BR](https://github.com/dfop02/manga-reader/README.pt-BR.md)
+
+### Como testar?
+
+Em breve pretendo simplificar o teste... Por enquanto você pode testar dessa forma:
+Baixe qualquer manga e adicione a pasta manga dentro do manga-reader, pegue o nome das imagens, elas precisam ser todas iguais diferenciando apenas o numero da página: 1.filename.jpg, 2.filename.jpg, etc.
+Porfim, no arquivo main.js, edite a linha 7 com o total de páginas e a linha 14 com o nome correto dos arquivos, abra o window.html no navegador e agora só testar!
+
+### Por que?
+
+Esse código foi feito baseado nas minhas próprias experiências e a dos meus amigos consumindo mangas, absorvendo as melhores e piores práticas de todas as ferramentas, o objetivo é construir uma boa ferramenta que os usuários podem facilmente usar e apreciar.
+
+### TO-DO
+
+- Construir a versão mobile
+- Ser capaz de ler PDF também
+
+### Autores
+
+* [Diogo Fernandes](https://github.com/dfop02)
+
+### Licença
+
+Esse projeto é licenciado sobre a licença MIT - veja o arquivo [LICENSE](LICENSE) para mais detalhes.
--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
+// Default Settings
 let current_page_number = 0;
 let current_size = 60;
 let current_style = 'vertical-style';
-let total_pages = 67;
-let manga_name = 'rawkuma';
 
-function collect_pages(total_pages=0) {
-  let pages = [];
-  for (let page in [...Array(total_pages).keys()]) {
-    pages.push(`manga/${page}.${manga_name}.com.jpg`);
-  }
+// User Settings
+let total_pages = 10;
+let manga_name = 'Manga Reader Example';
+let filenames = 'example.jpg';
 
-  $('.page-number span.total').html(total_pages);
-
-  return pages;
+function range(size, startAt = 0) {
+  return [...Array(size).keys()].map(i => i + startAt);
 }
 
-function show_image(id='', src='', width=current_size) {
-  $('.page').append(`<img id="${id}" width="${width}%" src="${src}"/>`)
+function collect_pages(total_pages = 0) {
+  return range(total_pages).map(page => `manga/${page}.${filenames}`);
+}
+
+function show_image(id = '', src = '', width = current_size) {
+  $('.page').append(`<img id="${id}" width="${width}%" src="${src}"/>`);
+}
+
+function generate_carousel() {
+  for (let page in range(total_pages)) {
+    $('.carousel-pages').append(`<span id="page${page}">${page}</span>`);
+  }
+}
+
+function mark_page_as_read(page_numbers = [], next_page = true) {
+  for (let number of page_numbers.values()) {
+    $(`#page${number}`).addClass('read');
+  }
+
+  if (next_page) {
+    current_page_number++;
+    $('.page-number input.current').val(current_page_number);
+  }
 }
 
 function update_page_size(new_size) {
-  $('.page img').each(function(){
-    $(this).attr('width', `${new_size}%`);
-  });
+  $('.page img').attr('width', `${new_size}%`);
 
   if ($('#pageSize').val() != new_size) {
-    $('#pageSize').val(new_size)
+    $('#pageSize').val(new_size);
   }
 }
 
@@ -33,27 +49,22 @@ function show_pages_by_style(style) {
   let pages = $('.page img');
 
   if (style == 'one-page-style') {
-    pages.each(function() {
-      console.log(`id: ${parseInt(this.id)} | current_page_number: ${current_page_number}`)
-      if (parseInt(this.id) != current_page_number) {
-        $(this).hide();
-      } else {
-        $(this).show();
-      }
-    });
+    $(`#${current_page_number}`).show();
+    pages.not(`#${current_page_number}`).hide();
   } else if (style == 'double-page-style') {
-    pages.each(function() {
-      if (parseInt(this.id) != current_page_number && parseInt(this.id) != current_page_number+1) {
-        $(this).hide();
-      } else {
-        $(this).show();
-      }
-    });
+    $(`#${current_page_number}`).show();
+    $(`#${current_page_number + 1}`).show();
+    pages.not(`#${current_page_number}`).not(`#${current_page_number + 1}`).hide()
   } else {
-    pages.each(function() {
-      $(this).show();
-    });
+    pages.show();
   }
+}
+
+function vertical_jump_to_page() {
+  console.log('page number: ' + current_page_number)
+  $('html, body').animate({
+    scrollTop: $(`#${current_page_number}`).offset().top
+  }, 500);
 }
 
 function toggle_page_buttons(hide=true) {
@@ -67,21 +78,22 @@ function toggle_page_buttons(hide=true) {
 }
 
 function switch_style(new_style) {
-  let page = $('.page');
-  let page_styles = ['one-page-style', 'double-page-style'];
+  const page = $('.page');
+  const page_styles = ['one-page-style', 'double-page-style'];
+  const vertical_styles = ['vertical-style', 'double-vertical-style'];
 
   if (current_style == new_style) return;
 
   if (page_styles.includes(current_style) && !page_styles.includes(new_style)) {
-    toggle_page_buttons(hide=true);
+    toggle_page_buttons();
     update_page_size(current_size);
   }
 
   if (new_style == 'one-page-style') {
-    toggle_page_buttons(hide=false);
+    toggle_page_buttons(false);
     update_page_size(current_size);
   } else if (new_style == 'double-page-style') {
-    toggle_page_buttons(hide=false);
+    toggle_page_buttons(false);
     update_page_size(50);
   } else if (new_style == 'double-vertical-style') {
     update_page_size(50);
@@ -90,10 +102,15 @@ function switch_style(new_style) {
   show_pages_by_style(new_style);
   page.removeClass(current_style);
   page.addClass(new_style);
+
+  if (vertical_styles.includes(current_style) && vertical_styles.includes(new_style)) {
+    vertical_jump_to_page();
+  }
+
   current_style = new_style;
 }
 
-function joinFullScreen(){
+function join_full_screen() {
   if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
     if (document.documentElement.requestFullscreen) {
       document.documentElement.requestFullscreen();
@@ -107,7 +124,7 @@ function joinFullScreen(){
   }
 }
 
-function exitFullScreen(){
+function exit_full_screen() {
   if (document.exitFullscreen) {
     document.exitFullscreen();
   } else if (document.msExitFullscreen) {
@@ -119,14 +136,21 @@ function exitFullScreen(){
   }
 }
 
+function switch_mode(isDarkMode) {
+  $('html').toggleClass('dark-mode');
+  $('.manga-box').toggleClass('dark-mode');
+}
+
 function move_page(direction) {
   if (direction == 'right') {
-    if (current_page_number < total_pages) {
-      if (current_style == 'double-page-style') {
-        current_page_number += 2;
-      } else {
-        current_page_number++;
-      }
+    if (current_page_number >= total_pages) return;
+
+    if (current_style == 'double-page-style') {
+      mark_page_as_read([current_page_number, current_page_number+1], false);
+      current_page_number += 2;
+    } else {
+      mark_page_as_read([current_page_number], false);
+      current_page_number++;
     }
   } else {
     if (current_style == 'double-page-style') {
@@ -144,25 +168,19 @@ function move_page(direction) {
 }
 
 $(function() {
-  let pages = collect_pages(total_pages=total_pages);
+  const page_styles = ['one-page-style', 'double-page-style'];
+  const vertical_styles = ['vertical-style', 'double-vertical-style'];
+  let isMobile = ( window.innerWidth <= 1000 );
+
+  // Booting
+  const pages = collect_pages(total_pages=total_pages);
 
   for (let [index, page] of pages.entries()) {
     show_image(id=index, src=page);
   }
 
-  // Fullscreen
-  let isFullscreen = false;
-
-  $('.fullscreen').click(function() {
-    if (isFullscreen) {
-      isFullscreen = false;
-      exitFullScreen();
-    } else {
-      isFullscreen = true;
-      joinFullScreen();
-    }
-    $(this).find('span').toggle();
-  });
+  $('.manga-title').html(manga_name);
+  $('.page-number span.total').html(total_pages);
 
   // Size Page
   $('#pageSize').change(function() {
@@ -178,14 +196,14 @@ $(function() {
   // Previous and Next Buttons
   $('.previous-page-btn').click(function() {
     move_page('left');
-  })
+  });
 
   $('.next-page-btn').click(function() {
     move_page('right');
-  })
+  });
 
   $(this).keydown(function(e) {
-    if (['one-page-style', 'double-page-style'].includes(current_style)) {
+    if (page_styles.includes(current_style)) {
       if (e.keyCode == 37) {
         move_page('left');
       }
@@ -195,31 +213,110 @@ $(function() {
     }
   });
 
+  // Mobile touch
+  if (isMobile) {
+    $(this).on('touchstart', function(e) {
+      let canMovePage = true;
+      let swipe = e.originalEvent.touches;
+      let start = swipe[0].pageX;
+
+      $(this).on('touchmove', function(e) {
+        let contact = e.originalEvent.touches;
+        let end = contact[0].pageX;
+        let distance = end-start;
+
+        if (canMovePage) {
+          if (distance >= 100) {
+            move_page('left');
+            canMovePage = false;
+          }
+          if (distance <= -100) {
+            move_page('right');
+            canMovePage = false;
+          }
+        }
+      })
+      .one('touchend', function() {
+        $(this).off('touchmove touchend');
+      });
+    });
+  }
+
+  // Fullscreen
+  let isFullscreen = false;
+
+  $('.fullscreen').click(function() {
+    if (isFullscreen) {
+      isFullscreen = false;
+      exit_full_screen();
+    } else {
+      isFullscreen = true;
+      join_full_screen();
+    }
+    $(this).find('span').toggle();
+  });
+
+  // Switch Mode
+  let isDarkMode = false;
+
+  $('.switch-mode').click(function() {
+    let mode = $(this).find('p.name-mode');
+
+    if (isDarkMode) {
+      isDarkMode = false;
+      mode.html('Dark Mode');
+    } else {
+      isDarkMode = true;
+      mode.html('Light Mode');
+    }
+
+    $(this).find('span').toggle();
+    switch_mode(isDarkMode);
+  })
+
   // Scroll Button and Header
   let btn = $('#scroll-top');
 
   $(window).scroll(function() {
     let scrollTop = $(this).scrollTop();
     let controls = $('.controls');
+    let carousel = $('.carousel-pages');
 
-    if (['vertical-style', 'double-vertical-style'].includes(current_style)) {
+    if (vertical_styles.includes(current_style)) {
       if (scrollTop > 300) {
         btn.fadeIn(1000);
         controls.addClass('fixed');
+        carousel.addClass('active');
+
+        if (!$('html,body').is(':animated') && scrollTop > $(`#${current_page_number}`).offset().top) {
+          mark_page_as_read([current_page_number]);
+        }
       } else {
         btn.fadeOut(1000);
         controls.removeClass('fixed');
+        carousel.removeClass('active');
       }
     } else {
       if (controls.hasClass('fixed')) {
         btn.fadeOut(1000);
         controls.removeClass('fixed');
+        carousel.removeClass('active');
       }
     }
   });
 
   btn.click(function(e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+    $('html, body').animate({scrollTop:0}, 300);
+  });
+
+  // Carousel
+  generate_carousel();
+
+  $('.carousel-pages span').click(function(e) {
+    e.preventDefault();
+    let number = $(this).html();
+    current_page_number = parseInt(number);
+    vertical_jump_to_page()
   });
 });
